52747871fd8b60c55a88f7cfb2bceda9aded75c2
whitebird1016 <takus.superdev@gmail.com>
2023-01-10 02:09:02 +0900

    add test.js

--- src/components/test.js
+++ src/components/test.js
@@ -2,12 +2,17 @@ import StellarSdk from "stellar-sdk";
 
 const Test = () => {
   // Replace with your own horizon server URL
-  const horizon = "https://horizon.stellar.org";
   // Replace with the recent ledger number that the user entered
-  const recentLedgerNumber = "339988";
+  const recentLedgerNumber = "40861697";
 
   // Replace with the five account numbers that the user entered
-  const accounts = "GAHUQ47QPCIU54OWKLZKOG2WRT7IXEJOJLDREQRGLNF2SOPQN7QG55HD";
+  const accounts = [
+    "GB6O5KIWNM5VMK6NYYV573PTDT7ZJBHXWLBBJXTY4LZDTC47Q2R4RX6S",
+    "GC4ML73EFGGHBWLITBMMNWSZK5PQK6TEHT2RUZZ7FTA3QUO4RAW7BWAD",
+    "GDWNMXOVH3GDYXFBD66U5OCHO33SUOSY37O6ZSZBCG33IZ7N6ZONZGFW",
+    "GA6KFS3IIG462BHDZCPKW3F3OHJ6Z2GY5JCLU6OPOSXSQYBKL5DTGSID",
+    "GCTW6KJYUJRQVTKQDMWH4EBT33DMGO2ZNLUDQIPS4KSZ6UXQAT2DAFHU",
+  ];
 
   // Replace with the scarcity level that the user entered
   const scarcityLevel = 50;
@@ -19,49 +24,39 @@ const Test = () => {
   const endOf1990s = 946684799;
 
   // Date on which transactions should be considered "Available Art"
-  const availableArtDate = new Date("2022-12-23").toJSON();
-  const availableArtDate2 = new Date("2022-12-27").toJSON();
+  const availableArtDate = new Date("May 14, 2022").toDateString();
   // Function to filter transactions that meet the criteria for "Available Art"
   const isAvailableArt = (transaction) => {
     // Check if the transaction took place on May 13, 2022 or has a mintime between the start and end of the 1990s
-    const isCorrectDate =
-      (transaction.created_at <= availableArtDate2 &&
-        transaction.created_at >= availableArtDate) ||
-      (transaction.mintime >= startOf1990s &&
-        transaction.mintime <= endOf1990s);
-    // Check if the transaction is for the correct amount of Stroop
-    const isCorrectAmount = transaction.amount === scarcityLevel + 50;
-
-    // Return true if both conditions are met, false otherwise
-    return isCorrectDate;
   };
 
   // Function to filter out transactions with duplicate memos
-  // const filterDuplicateMemos = (transactions) => {
-  //   // Set to store unique memos
-  //   const uniqueMemos = new Set();
-  //   // Filter out transactions with duplicate memos
-  //   return transactions.filter((transaction) => {
-  //     // Check if the memo is blank or consists only of spaces
-  //     if (transaction.memo.trim() === "") {
-  //       return false;
-  //     }
+  const filterDuplicateMemos = (transactions) => {
+    // Set to store unique memos
+    const uniqueMemos = new Set();
 
-  //     // Check if the memo has already been seen
-  //     if (uniqueMemos.has(transaction.memo)) {
-  //       return false;
-  //     }
-  //     // Add the memo to the set of unique memos
-  //     uniqueMemos.add(transaction.memo);
-  //     return true;
-  //   });
-  // };
+    // Filter out transactions with duplicate memos
+    return transactions.filter((transaction) => {
+      // Check if the memo is blank or consists only of spaces
+      if (transaction.memo.trim() === "") {
+        return false;
+      }
+
+      // Check if the memo has already been seen
+      if (uniqueMemos.has(transaction.memo)) {
+        return false;
+      }
+
+      // Add the memo to the set of unique memos
+      uniqueMemos.add(transaction.memo);
+      return true;
+    });
+  };
 
   // Function to remove delisted art from the "Available Art" list
   const removeDelistedArt = (transactions) => {
     // Map to store the delisted memos
     const delistedMemos = new Map();
-
     // Iterate through the transactions and store the delisted memos
     transactions.forEach((transaction) => {
       // Check if the transaction is a delisting transaction
@@ -84,31 +79,89 @@ const Test = () => {
     });
   };
 
+  // // Connect to the Horizon server
+  // const server = new StellarSdk.Server(horizon);
+
+  // // Query the Horizon server for the transactions in the specified accounts and ledger range
+  // server
+  //   .transactions()
+  //   .forAccounts(accounts)
+  //   .ledger(recentLedgerNumber)
+  //   .order("asc") // order the transactions in ascending order
+  //   .call()
+  //   .then((results) => {
+  //     // Filter the transactions to include only those that meet the criteria for "Available Art"
+  //     const availableArtTransactions = results.records.filter(isAvailableArt);
+
+  //     // Remove delisted art from the "Available Art" list
+  //     const filteredTransactions = removeDelistedArt(availableArtTransactions);
+
+  //     // Sort the remaining transactions in operation order
+  //     const sortedTransactions = filteredTransactions.sort(
+  //       (a, b) => a.operation_count - b.operation_count
+  //     );
+
+  //     // Print the "Available Art" list
+  //     printAvailableArt(sortedTransactions);
+  //   });
+
   // Connect to the Horizon server
-  const server = new StellarSdk.Server("https://horizon-testnet.stellar.org");
+  const server = new StellarSdk.Server("https://horizon.stellar.org");
 
   server
     .transactions()
-    .forAccount(accounts)
+    .forLedger(recentLedgerNumber)
+    .limit("200")
     .call()
     .then(function (results) {
-      const availableArtTransactions = results.records.filter(isAvailableArt);
+      for (let i = 0; i < accounts.length; i++) {
+        const availableArtTransactions = results.records.filter(
+          (transaction) => {
+            const date = new Date(transaction.created_at).toDateString();
+            const isCorrectDate =
+              date === availableArtDate ||
+              (transaction.mintime >= startOf1990s &&
+                transaction.mintime <= endOf1990s);
+            const isCorrectAccount = transaction.source_account === accounts[i];
+            console.log(accounts[i], "SA");
+            // Check if the transaction is for the correct amount of Stroop
+            // const isCorrectAmount = transaction.amount === scarcityLevel + 50;
+            // Return true if both conditions are met, false otherwise
+
+            return isCorrectDate && isCorrectAccount;
+          }
+        );
+
+        // Remove delisted art from the "Available Art" list
+        const filteredTransactions = removeDelistedArt(
+          availableArtTransactions
+        );
+        const sortedTransactions = filteredTransactions.sort(
+          (a, b) => a.operation_count - b.operation_count
+        );
+        console.log(availableArtTransactions, "availableArtTransactions");
+        printAvailableArt(sortedTransactions);
+      }
+
+      return results.next();
+      // // Sort the remaining transactions in operation order
+
+      // // Print the "Available Art" list
+    })
+    .then(function (result) {
+      const availableArtTransactions = result.records.filter(isAvailableArt);
       // Remove delisted art from the "Available Art" list
       const filteredTransactions = removeDelistedArt(availableArtTransactions);
-      // Sort the remaining transactions in operation order
-      const sortedTransactions = filteredTransactions.sort(
-        (a, b) => a.operation_count - b.operation_count
-      );
-      // Print the "Available Art" list
+      const sortedTransactions = filterDuplicateMemos(
+        filteredTransactions
+      ).sort((a, b) => a.operation_count - b.operation_count);
+      console.log(availableArtTransactions, "availableArtTransactions");
       printAvailableArt(sortedTransactions);
     })
     .catch(function (err) {
       console.log(err);
     });
-  checkBillOwnership(
-    "fa19c57af52f7e3bd66585e0318fb659bf1c47b67c25307d424de93c825593c7",
-    accounts
-  );
+
   async function checkBillOwnership(serialNumber, recipientAccountId) {
     // Load the transaction with the specified serial number
     const transaction = await server
